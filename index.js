6a54caf6b59fcd2c5eacb808a873839bb52c6edf
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2023-02-01 10:33:16 +0200

    Change port

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.port || 3000;
 server.listen(port, () => {
   console.log("Server is running...");
 });
